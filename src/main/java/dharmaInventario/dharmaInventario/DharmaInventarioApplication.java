cbcbf8276c5f4a51fd29c6bcfa61d386bdeebb2b
Daniela Montoya Quintero <daniela.quintero@Quipux.local>
2025-12-03 18:49:01 -0500

    imprimir variables

--- src/main/java/dharmaInventario/dharmaInventario/DharmaInventarioApplication.java
+++ src/main/java/dharmaInventario/dharmaInventario/DharmaInventarioApplication.java
@@ -9,5 +9,8 @@ public class DharmaInventarioApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DharmaInventarioApplication.class, args);
         System.out.println("Levantando proyecto");
+		System.out.println("SPRING_DATASOURCE_URL = " + System.getenv("SPRING_DATASOURCE_URL"));
+		System.out.println("SPRING_DATASOURCE_USERNAME = " + System.getenv("SPRING_DATASOURCE_USERNAME"));
+		System.out.println("SPRING_DATASOURCE_PASSWORD = " + System.getenv("SPRING_DATASOURCE_PASSWORD"));
 	}
 }
