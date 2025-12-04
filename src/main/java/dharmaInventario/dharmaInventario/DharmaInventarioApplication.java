f2f0e46f863ab62184559d82b2f4798beb7db04e
Daniela Montoya Quintero <daniela.quintero@Quipux.local>
2025-12-03 19:25:44 -0500

    agregar impresion de variable

--- src/main/java/dharmaInventario/dharmaInventario/DharmaInventarioApplication.java
+++ src/main/java/dharmaInventario/dharmaInventario/DharmaInventarioApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DharmaInventarioApplication {
 
 	public static void main(String[] args) {
+		System.out.println("SPRING_DATASOURCE_URL = " + System.getenv("SPRING_DATASOURCE_URL"));
 		SpringApplication.run(DharmaInventarioApplication.class, args);
         System.out.println("Levantando proyecto");
 	}
